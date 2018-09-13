1e9b1215b706bc4b81145431927c01b33e525183
Ahamad-PC <ahamad.jedu@gmail.com>
2018-09-14 05:40:59 +0700

    update view

--- views/default/view.php
+++ views/default/view.php
@@ -8,10 +8,9 @@ use yii\web\UploadedFile;
 /* @var $this yii\web\View */
 /* @var $model andahrm\edoc\models\Edoc */
 
-$this->title = $model->code." ".$model->title;
+$this->title = $model->code . " " . $model->title;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('andahrm/edoc', 'Edocs'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
-
 ?>
 <div class="edoc-view">
 
@@ -19,37 +18,40 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <p>
         <?= Html::a(Yii::t('andahrm', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a(Yii::t('andahrm', 'Delete'), ['delete', 'id' => $model->id], [
+        <?=
+        Html::a(Yii::t('andahrm', 'Delete'), ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => Yii::t('andahrm', 'Are you sure you want to delete this item?'),
                 'method' => 'post',
             ],
-        ]) ?>
+        ])
+        ?>
     </p>
 
-    
-  
-    <?php
-if(preg_match('/\.pdf$/i', $model->file)){
-    echo \yii2assets\pdfjs\PdfJs::widget([
-      'url' => Url::base().'/..'.$model->getUploadUrl('file'),
-      'buttons'=>[
-        'presentationMode' => false,
-        'openFile' => false,
-        'print' => true,
-        'download' => true,
-        'viewBookmark' => false,
-        'secondaryToolbarToggle' => false,
-        'fullscreen'=>true,
-      ]
-    ]);
-}elseif($model->file){
-    echo Html::img($model->getUploadUrl('file', 'preview'), ['class' => 'img-thumbnail','width'=>"100%"])."<br/><br/>";
-}
-   ?>
 
-  <?= DetailView::widget([
+
+    <?php
+    if (preg_match('/\.pdf$/i', $model->file)) {
+        echo \yii2assets\pdfjs\PdfJs::widget([
+            'url' => Url::base() . '/..' . $model->getUploadUrl('file'),
+            'buttons' => [
+                'presentationMode' => false,
+                'openFile' => false,
+                'print' => true,
+                'download' => true,
+                'viewBookmark' => false,
+                'secondaryToolbarToggle' => false,
+                'fullscreen' => true,
+            ]
+        ]);
+    } elseif ($model->file) {
+        echo Html::img($model->getUploadUrl('file', 'preview'), ['class' => 'img-thumbnail', 'width' => "100%"]) . "<br/><br/>";
+    }
+    ?>
+
+    <?=
+    DetailView::widget([
         'model' => $model,
         'attributes' => [
             //'id',
@@ -58,10 +60,11 @@ if(preg_match('/\.pdf$/i', $model->file)){
             'title',
             'file',
             'file_name',
-            'created_at',
+            'created_at:datetime',
             'created_by',
             'updated_at',
-            'updated_by',
+            'updated_by:datetime',
         ],
-    ]) ?>
+    ])
+    ?>
 </div>
